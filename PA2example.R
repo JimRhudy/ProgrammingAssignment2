8a1aa3f50a7b0f433a9e3ef1cdfcdd7c7ccf2c9b
Jim Rhudy <jrhudy@hotmail.com>
2017-08-20 18:35:46 -0500

    PA2 submission

---
+++ PA2example.R
@@ -0,0 +1,36 @@
+# PA2 example
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+  
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+x <- 1:20
+
+z <- makeVector(x)
+z
+
+cachemean(z)
+
+cachemean(z)
--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this code would support a use case of matrix inversion
+## author: Jim Rhudy, 8/20/2017
 
-## Write a short comment describing this function
+## This function defines a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  # set the matrix
+  set<-function(y) {
+    x<<- y
+    m<<- NULL
+  }
+  # get the matrix
+  get<-function() x
+  # set value of inverse
+  setinverse<-function(solve) m<<-solve
+  # get value of inverse
+  getinverse <-function() m
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function inverts a matrix if it has not already been 
+## inverted and if the matrix has not changed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # check if matrix has been inverted
+  m<-x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  # invert the matrix
+  data<-x$get()
+  m<-solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+# here is some code used to test the functions
+# # a<-c(8, 2, 4)
+# b<-c(8, 0, 5)
+# c<-c(8, 2, 0)
+# d<-cbind(a, b, c)
+# d
+# ds<-solve(d)
+# ds
+# makeCacheMatrix(d)
+# cacheSolve(makeCacheMatrix(d))
+
